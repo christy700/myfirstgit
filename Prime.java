5ae696f8ccafcee191988635fbd3aaa8e81f31e2
christy700 <josechristy800@gmail.com>
2019-03-12 12:04:57 +0530

    4th commit

---
+++ Prime.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Prime
+{
+    public static void main(String[] args) {
+        for(int i=2;i<100;i++)
+        {
+            for(int j=2;j<i;j++)
+            {
+                if(i%j==0)
+                    break;
+                else if(i==j+1)
+                System.out.println("\n" +i);
+            }
+        }
+    }
+}
+    
