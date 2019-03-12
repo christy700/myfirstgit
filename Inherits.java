5b05c202914c3fb03e373125c3e3ea572a514719
christy700 <josechristy800@gmail.com>
2019-03-12 14:19:28 +0530

    8th commit

--- Inherits.java
+++ Inherits.java
@@ -10,16 +10,25 @@ class B extends A
  public void bike()
  {
     System.out.println("bike");
- }
- 
  
 }
-public class Inherit
+ }
+ class C extends B
+{
+ public void cycle()
+ {
+    System.out.println("mtb");
+ 
+}
+}
+public class Inherits
 {
     public static void main(String[] args) 
     {
-        B d=new B();
+        C d=new C();
         d.bike();
         d.car();
+        d.cycle();
     }
 }
+ 
